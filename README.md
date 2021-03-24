685edc0c0ba10ec206d685b3fb788dbfa12c8f00
Paul <paul.hegenberg@gmail.com>
2021-03-24 20:50:42 +0100

    Adding additional arguments for command execution - "some" cleanup

--- README.md
+++ README.md
@@ -4,14 +4,15 @@ Simple testing tool for commandline based programs
 
 > PS: the code looks really messy because I'm in a hurry, if anyone wants to clean it up, feel free
 
-
-
 ## Testing
 
 To test a commandline program, simply create a textfile containing the user dialog as following:
 
-````
+````bash
+$$arg1 arg2
 # this is a comment that will be ignored
+# in the first line you can define additional arguments for command execution
+
 # everything starting with > will be sent to the program over std in
 # example:
 >command-to-pass
@@ -25,17 +26,13 @@ $[0-9]+
 
 > test01.txt
 
-
-
 To run the the test simply execute the following command on the commandline:
 
-````
+````bash
 testor -testFile="./examples/test01.txt" programToTest.exe argumentForTestProgram
 ````
 
 If you want to test multiple files you may want to create a script for that.
-
-
 
 ## Parameters
 
@@ -48,3 +45,4 @@ The program has several parameters:
 | cmdPrefix      | prefix for a command that is passed to the program to test (default: >) |
 | regexPrefix    | prefix for regex interpretation (default: $)                 |
 | commentPrefix  | prefix for comments which are not interpreted (default: #)   |
+| argsPrefix     | prefix for additional arguments for the command to execute, defined in the first line of the test file |
--- main.go
+++ main.go
@@ -14,12 +14,14 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+var testFileFlag = flag.String("testFile", "", "help message for flag n")
+var commandPrefixFlag = flag.String("cmdPrefix", ">", "line prefix for commands passed to the application")
+var regexPrefixFlag = flag.String("regexPrefix", "$", "prefix that indicates that the following line should be interpreted as a regex")
+var commentPrefixFlag = flag.String("commentPrefix", "#", "prefix used for comments that are not interpreted")
+var argsPrefixFlag = flag.String("argsPrefix", "$$", "define how the first line starts when passing args")
+var logLevelFlag = flag.String("logLevel", "info", "log level for the amount of information (debug, info)")
+
 func main() {
-	var testFileFlag = flag.String("testFile", "", "help message for flag n")
-	var logLevelFlag = flag.String("logLevel", "info", "log level for the amount of information (debug, info)")
-	var commandPrefixFlag = flag.String("cmdPrefix", ">", "line prefix for commands passed to the application")
-	var regexPrefixFlag = flag.String("regexPrefix", "$", "prefix that indicates that the following line should be interpreted as a regex")
-	var commentPrefixFlag = flag.String("commentPrefix", "#", "prefix used for comments that are not interpreted")
 
 	flag.Parse()
 
@@ -29,14 +31,8 @@ func main() {
 		DisableTimestamp: true,
 	})
 
-	switch *logLevelFlag {
-	case "debug":
+	if *logLevelFlag == "debug" {
 		log.SetLevel(log.DebugLevel)
-		break
-	case "info":
-	default:
-		log.SetLevel(log.InfoLevel)
-		break
 	}
 
 	// open the file specified with the flag
@@ -54,28 +50,39 @@ func main() {
 		}
 	}()
 
-	// start the program
+	firstLine := ""
+
+	// read lines
+	fileScanner := bufio.NewScanner(file)
+	if fileScanner.Scan() {
+		firstLine = fileScanner.Text()
+	} else {
+		log.Fatal("File is empty.")
+	}
+
+	args := make([]string, 0)
+
+	// if the file starts with $$ in the first line
+	// read it as additional arguments
+	if strings.HasPrefix(firstLine, *argsPrefixFlag) {
+		argString := firstLine[len(*argsPrefixFlag):]
+		additionalFileArgs := strings.Split(argString, " ")
+
+		log.Debug("Args read from file: ", additionalFileArgs)
+
+		args = append(args, additionalFileArgs...)
+		firstLine = ""
+	}
+
+	// command to execute on the shell
 	command := flag.Arg(0)
-	log.Debug("Executing:", flag.Args())
+	args = append(flag.Args()[1:], args...)
 
-	cmd := exec.Command(command, flag.Args()[1:]...)
+	log.Debug("Executing:", args)
+	// start the program
+	cmd := exec.Command(command, args...)
 
-	inPipe, err := cmd.StdinPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	outPipe, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	outScanner := bufio.NewScanner(outPipe)
-
-	errPipe, err := cmd.StderrPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	errScanner := bufio.NewScanner(errPipe)
+	outScanner, errScanner, inPipe := applicationStdInterface(cmd)
 
 	go func() {
 		for errScanner.Scan() {
@@ -90,64 +97,18 @@ func main() {
 
 	lineNumber := 1
 
-	// read lines
-	fileScanner := bufio.NewScanner(file)
+	if len(firstLine) > 0 {
+		checkLine(firstLine, lineNumber, outScanner, &inPipe)
+		lineNumber++
+	}
+
 	for fileScanner.Scan() {
 
 		// line from the file
 		var line = fileScanner.Text()
-
 		log.Debug("Reading line:", line)
 
-		// ignore comment files
-		if strings.HasPrefix(line, *commentPrefixFlag) {
-			continue
-		}
-
-		// check if the line is a command
-		if strings.HasPrefix(line, *commandPrefixFlag) {
-			var lineWithoutPrefix = line[len(*commandPrefixFlag):]
-
-			log.Debug("Passing command:", lineWithoutPrefix)
-
-			_, err = io.WriteString(inPipe, lineWithoutPrefix+"\n")
-			if err != nil {
-				log.Fatal("Error passing command:", err)
-			}
-
-		} else {
-
-			log.Debug("Trying to read response from std out...")
-
-			if outScanner.Scan() {
-				// read the response line from the program
-				response := outScanner.Text()
-
-				log.Debug("Reading response:", response)
-
-				if strings.HasPrefix(line, *regexPrefixFlag) {
-					regexWithoutPrefix := line[len(*regexPrefixFlag):]
-
-					matched, err := regexp.MatchString(regexWithoutPrefix, response)
-					if err != nil {
-						log.Fatal("Error matching regex: ", err)
-					}
-
-					if !matched {
-						logMismatch(regexWithoutPrefix, response, lineNumber)
-					}
-
-				} else {
-					// check if the line equals the passed line
-					if response != line {
-						logMismatch(line, response, lineNumber)
-					}
-				}
-
-			} else {
-				log.Fatal("No response from the program, line:", lineNumber)
-			}
-		}
+		checkLine(line, lineNumber, outScanner, &inPipe)
 
 		lineNumber++
 	}
@@ -159,6 +120,80 @@ func main() {
 	log.Info(emoji.Sprint(":check_mark: All tests passed"))
 }
 
+func applicationStdInterface(cmd *exec.Cmd) (outScanner, errScanner *bufio.Scanner, inPipe io.WriteCloser) {
+	inPipe, err := cmd.StdinPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	outPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	outScanner = bufio.NewScanner(outPipe)
+
+	errPipe, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	errScanner = bufio.NewScanner(errPipe)
+	return
+}
+
 func logMismatch(expected, value string, lineNumber int) {
 	log.Fatal(emoji.Sprint(":white_exclamation_mark: Error on line ", lineNumber, " expected: ", expected, " got: ", value))
+}
+
+func checkLine(line string, lineNumber int, outScanner *bufio.Scanner, inPipe *io.WriteCloser) {
+	// ignore comment files
+	if strings.HasPrefix(line, *commentPrefixFlag) {
+		return
+	}
+
+	// check if the line is a command
+	// then passing it to the running application
+	if strings.HasPrefix(line, *commandPrefixFlag) {
+		var lineWithoutPrefix = line[len(*commandPrefixFlag):]
+
+		log.Debug("Passing command:", lineWithoutPrefix)
+
+		_, err := io.WriteString(*inPipe, lineWithoutPrefix+"\n")
+		if err != nil {
+			log.Fatal("Error passing command:", err)
+		}
+
+		return
+	}
+
+	log.Debug("Trying to read response from std out...")
+
+	if outScanner.Scan() {
+		// read the response line from the program
+		response := outScanner.Text()
+
+		log.Debug("Reading response:", response)
+
+		if strings.HasPrefix(line, *regexPrefixFlag) {
+			regexWithoutPrefix := line[len(*regexPrefixFlag):]
+
+			matched, err := regexp.MatchString(regexWithoutPrefix, response)
+			if err != nil {
+				log.Fatal("Error matching regex: ", err)
+			}
+
+			if !matched {
+				logMismatch(regexWithoutPrefix, response, lineNumber)
+			}
+
+		} else {
+			// check if the line equals the passed line
+			if response != line {
+				logMismatch(line, response, lineNumber)
+			}
+		}
+
+	} else {
+		log.Fatal("No response from the program, line:", lineNumber)
+	}
+
 }
